32de337c07a910f89ff955c830d2b531740178a3
Сергій Власов <vikinglaurasun@gmail.com>
2025-08-02 20:05:10 +0300

    fix noteform naming

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -2,7 +2,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createNote } from '../../lib/api';
-import { type Note } from '../../lib/api';
+import { type Note } from '../../types/note';
 import css from './NoteForm.module.css';
 
 interface NoteFormProps {
