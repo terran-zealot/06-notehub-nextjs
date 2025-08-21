7bdc357e0bb7f758fc71c1d60c6ea14a65df5c8c
Сергій Власов <vikinglaurasun@gmail.com>
2025-08-21 18:17:47 +0300

    trying fix naming

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TanStackProvider from "../components/TanStackProvider/TanStackProvider";
 import Header from "../components/Header/Header";
-import Footer from "@/components/Footer/Footer";
+import Footer from "../components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
